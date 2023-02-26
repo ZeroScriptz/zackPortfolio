b71be519ae40c6c07854e0400d8d075971c7ee67
ZacksDesign <zackdgaming@gmail.com>
2023-02-26 13:25:24 -0800

    portion of navBar.jsx component complete
    This completes part of the navBar component, not the entire thing.
    
    Need to finish the toggle menu options for mobile devices including the 2 icons that will show.

--- devPortfolio/src/components/navBar.jsx
+++ devPortfolio/src/components/navBar.jsx
@@ -3,3 +3,36 @@ import {AiOutlineClose} from 'react-icons';
 import { useState } from 'react';
 import logo from '../assets/images/logoMain.png';
 
+const NavbarItem = ({ title, classProps  }) => {
+    return (
+        <li className={`mx-4 cursor-pointer ${classProps}`}>
+            {title}
+        </li>
+    )
+}
+
+const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+    return (
+        <nav className='w-full items-center p-4 md:justify-center'>
+            <div className='flex-intial justify-center items-center md:flex-[0.5]'>
+                <img 
+                src={logo}
+                alt='Zack darnell logo'
+                className='w-32 cursor-pointer'
+                />
+            </div>
+            <ul className='text-white list-none flex-row justify-between items-center flex-intial md:flex'>
+                {["Home", "Projects", "About", "Contact"].map((item, index) => (
+                    <NavbarItem key={item + index} title={item} />
+                ))}
+            </ul>
+            <div className='flex relative'>
+
+            </div>
+        </nav>
+    )
+
+
+
+}
