5705489463fd06cd897bbc33885bf7ac4fe27d71
ZacksDesign <zackdgaming@gmail.com>
2023-02-27 10:30:01 -0800

    fixed most mobile device issues
    mobile device visibility issues were fixed, contemplating fixing the X mark for both fields. though I can focus more time on that later.

--- devPortfolio/vite.config.js
+++ devPortfolio/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import liveReload from 'vite-plugin-live-reload'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [liveReload('.path'), react()],
+  server: {
+    hmr: true
+  }
 })
